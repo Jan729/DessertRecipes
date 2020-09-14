6d72a6c09b63a4679d1559be677228a2898e4f6d
Jan729 <46268176+Jan729@users.noreply.github.com>
2020-09-14 18:39:06 -0400

    Updates

---
+++ precache-manifest.22755abb9e4e1ef67c9d5b950c9449d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071abe6f19ad1fff6fa92368690924fc",
+    "url": "/DessertRecipes/index.html"
+  },
+  {
+    "revision": "88e6f852168ac947cc2a",
+    "url": "/DessertRecipes/static/css/2.06c478a6.chunk.css"
+  },
+  {
+    "revision": "29cae135a475d9176065",
+    "url": "/DessertRecipes/static/css/main.b2429e2e.chunk.css"
+  },
+  {
+    "revision": "88e6f852168ac947cc2a",
+    "url": "/DessertRecipes/static/js/2.e28dfc1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DessertRecipes/static/js/2.e28dfc1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29cae135a475d9176065",
+    "url": "/DessertRecipes/static/js/main.1b528cbd.chunk.js"
+  },
+  {
+    "revision": "423927ae48ed043faa5f",
+    "url": "/DessertRecipes/static/js/runtime-main.93623328.js"
+  }
+]);
