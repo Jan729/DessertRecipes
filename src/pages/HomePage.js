b46639af726ab8a4b0d2b8c08ca25fe9e32ae0cf
Jan729 <46268176+Jan729@users.noreply.github.com>
2020-09-14 19:31:19 -0400

    Comment out unused recipe headings

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -15,11 +15,11 @@ const HomePage = () => {
 
       <div class="container p-3">
         <h1 className="display-4">Desserts</h1>
-              <RecipeTable recipeData={recipeJSON.desserts} category={"desserts"}/>
-        <h1 className="display-4">Real Food</h1>
+        <RecipeTable recipeData={recipeJSON.desserts} category={"desserts"} />
+        {/*<h1 className="display-4">Real Food</h1>
               <RecipeTable recipeData={recipeJSON.realFood} category={"realFood"}/>
         <h1 className="display-4">Sauces</h1>
-              <RecipeTable recipeData={recipeJSON.sauces} category={"sauces"}/>
+              <RecipeTable recipeData={recipeJSON.sauces} category={"sauces"}/> */}
       </div>
     </div>
   );
